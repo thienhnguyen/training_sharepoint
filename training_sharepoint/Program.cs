69a6c2e65a42fa272397380db8d47595c151ac80
Thien Nguyen Hoan <thien.nguyen.hoan@preciofishbone.se>
2020-09-04 17:24:13 +0700

    add content type to list

--- training_sharepoint/Program.cs
+++ training_sharepoint/Program.cs
@@ -17,9 +17,13 @@ namespace training_sharepoint
             string userName = "thienhnguyen@thienhnguyen.onmicrosoft.com";
             var password = "{S$t5rN$";
 
+            var listDocName = "SharePoint API";
+            var contentTypeName = "Project Documents Library";
+
             var securePassword = new SecureString();
             password.ToCharArray().ToList().ForEach(c => securePassword.AppendChar(c));
 
+            #region Authentication
             //using(var context = new ClientContext(site))
             //{
 
@@ -40,13 +44,14 @@ namespace training_sharepoint
             //}
 
             var authenticationManager = new OfficeDevPnP.Core.AuthenticationManager();
-
             ClientContext context = authenticationManager.GetWebLoginClientContext(site.ToString(), null);
 
+            #endregion
+
+            #region Create New Document List
             ListCreationInformation lci = new ListCreationInformation
             {
-                Description = "Library used to hold Dynamics CRM documents",
-                Title = "Test",
+                Title = listDocName,
                 TemplateType = 101,
 
             };
@@ -54,7 +59,29 @@ namespace training_sharepoint
             List lib = context.Web.Lists.Add(lci);
             lib.ContentTypesEnabled = true;
             lib.Update();
-            context.Load(lib);
+
+            #endregion
+
+            #region Add Content Type to List
+            ContentTypeCollection contentTypeCollection;
+
+            contentTypeCollection = context.Site.RootWeb.ContentTypes;
+
+            context.Load(contentTypeCollection);
+            context.ExecuteQuery();
+
+            ContentType targetContentType = (from c in contentTypeCollection
+                                             where c.Name == contentTypeName
+                                             select c).FirstOrDefault();
+
+            List targetList = context.Web.Lists.GetByTitle(listDocName);
+            targetList.ContentTypes.AddExistingContentType(targetContentType);
+            targetList.Update();
+
+            context.Web.Update();
+
+            #endregion
+
             context.ExecuteQuery();
         }
     }
